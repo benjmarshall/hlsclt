0ae9bb15796e14e0662b8a46e0b01f8aa70c3e06
Ben Marshall <ben@benmarshall.co.uk>
2017-06-20 17:54:54 +1200

    Added option to report command.

--- hlsclt/report_commands/report_commands.py
+++ hlsclt/report_commands/report_commands.py
@@ -9,7 +9,10 @@ import click
 
 ### Click Command Definitions ###
 # Report Command
-@click.command('report',short_help='Open reports.')
-def report():
+@click.command('report', short_help='Open reports.')
+@click.option('-s', '--stage', required=True, multiple=True,
+                type=click.Choice(['csim','syn','cosim','export']),
+                help='Which build stage to open the report for. Multiple occurances accepted')
+def report(stage):
     """Opens the Vivado HLS report for the chosen build stages."""
     click.echo("Report Mode")
