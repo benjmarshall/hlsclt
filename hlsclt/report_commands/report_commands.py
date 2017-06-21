6fe3f3245cafc025a32f0170d18538a6e2355de7
Ben Marshall <ben@benmarshall.co.uk>
2017-06-21 15:27:04 +1200

    Started project status command implementation.

--- hlsclt/report_commands/report_commands.py
+++ hlsclt/report_commands/report_commands.py
@@ -31,10 +31,10 @@ def open_report(ctx,report):
     elif report == 'cosim':
         report_files.append(config["project_name"] + "/solution" + str(solution_num) + "/sim/report/" + config["top_level_function_name"] + "_cosim.rpt")
         for language in just_loop_on(config["language"]):
-            report_files.append(config["project_name"] + "/solution" + str(solution_num) + "/sim/report/" + config["language"] + "/" + config["top_level_function_name"] + ".log")
+            report_files.append(config["project_name"] + "/solution" + str(solution_num) + "/sim/report/" + language + "/" + config["top_level_function_name"] + ".log")
     elif report == 'export':
         for language in just_loop_on(config["language"]):
-            report_files.append(config["project_name"] + "/solution" + str(solution_num) + "/impl/report/" + config["language"] + "/" + config["top_level_function_name"] + "_export.rpt")
+            report_files.append(config["project_name"] + "/solution" + str(solution_num) + "/impl/report/" + language + "/" + config["top_level_function_name"] + "_export.rpt")
     for file in report_files:
         return_val = os.system('xdg-open ' + file + ' >/dev/null 2>&1')
         if return_val != 0:
@@ -44,6 +44,49 @@ def open_report(ctx,report):
 def open_project_in_gui(ctx):
     config = ctx.obj.config
     hls_process = subprocess.Popen(["vivado_hls", "-p", config["project_name"]])
+
+# Function for finding the project status
+def gather_project_status(ctx):
+    config = ctx.obj.config
+    solution_num = ctx.obj.solution_num
+    project_status = []
+    if os.path.isfile(config["project_name"] + "/solution" + str(solution_num) + "/csim/report/" + config["top_level_function_name"] + "_csim.log"):
+        project_status.append("csim_done")
+    if os.path.isfile(config["project_name"] + "/solution" + str(solution_num) + "/syn/report/" + config["top_level_function_name"] + "_csynth.rpt"):
+        project_status.append('syn_done')
+    if os.path.isfile(config["project_name"] + "/solution" + str(solution_num) + "/sim/report/" + config["top_level_function_name"] + "_cosim.rpt"):
+        project_status.append('cosim_done')
+        cache = []
+        for line in click.open_file(config["project_name"] + "/solution" + str(solution_num) + "/sim/report/" + config["top_level_function_name"] + "_cosim.rpt"):
+            cache.append(line)
+            if "vhdl" in cache[line].lower():
+                if "pass" in cache[line].lower():
+                    project_status.append('cosim_vhdl_pass')
+                elif "fail" in cache[line].lower():
+                    project_status.append('cosim_vhdl_fail')
+            if "verilog" in cache[line].lower():
+                if "pass" in cache[line].lower():
+                    project_status.append('cosim_verilog_pass')
+                elif "fail" in cache[line].lower():
+                    project_status.append('cosim_verilog_fail')
+    if os.path.isdir(config["project_name"] + "/solution" + str(solution_num) + "/impl/ip"):
+        project_status.append('export_ip_done')
+    if os.path.isdir(config["project_name"] + "/solution" + str(solution_num) + "/impl/sysgen"):
+        project_status.append('export_sysgen_done')
+    for language in just_loop_on(config["language"]):
+        if os.path.isfile(config["project_name"] + "/solution" + str(solution_num) + "/impl/report/" + language + "/" + config["top_level_function_name"] + "_export.rpt"):
+            if language == "vhdl":
+                project_status.append('evaluate_vhdl_done')
+            elif language == "verilog":
+                project_status.append('evaluate_verilog_done')
+    return project_status
+
+
+# Function for printing out the project status
+def print_project_status(ctx):
+    project_status = gather_project_status(ctx)
+    click.echo(project_status)
+
 
 ### Click Command Definitions ###
 # Report Command
@@ -65,3 +108,11 @@ def open_gui(ctx):
     """Opens the Vivado HLS GUI and loads the project."""
     check_for_project(ctx)
     open_project_in_gui(ctx)
+
+@click.command('status', short_help='Print out the current project status.')
+@click.pass_context
+def status(ctx):
+    """Prints out a message detailing the current project status."""
+    check_for_project(ctx)
+    ctx.obj.solution_num = find_solution_num(ctx)
+    print_project_status(ctx)
