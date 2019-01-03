8248a14152c0e58619068b2ce7cb4748e07256df
Víctor Mayoral Vilches <v.mayoralv@gmail.com>
2019-01-03 20:39:45 +0100

    build command, remove leftover

--- hlsclt/build_commands/build_commands.py
+++ hlsclt/build_commands/build_commands.py
@@ -44,7 +44,6 @@ def do_start_build_stuff(ctx):
 def do_default_build(ctx):
     config = ctx.obj.config
     file = ctx.obj.file
-    solution_num = ctx.obj.solution_num
     file.write("csim_design -clean" + (" -compiler clang" if config.get("compiler","") == "clang" else "") + " \n")
     file.write("csynth_design" + "\n")
     file.write("cosim_design -O -rtl " + config["language"] + "\n")
